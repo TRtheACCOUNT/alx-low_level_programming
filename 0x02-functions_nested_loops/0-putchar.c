ac7ee280db7149f3c510664aabf75f80b2698eea
Tar <tarmh0559@gmail.com>
2023-02-21 09:25:40 -0800

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,19 @@
-#include <stdio.h>
+#include main.h
 /**
  * main - Print a phrase.
  * Return:0
  */
 int main(void)
 {
-	putchar ('_putchar\n');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
+
