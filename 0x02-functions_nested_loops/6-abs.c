232ea368ada1c237a78964c60aeca95b8fbed7b0
Tar <tarmh0559@gmail.com>
2023-02-21 16:06:10 -0800

    6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * int_abs - Gives the absolute value of a number .
+ * int _abs - Gives the absolute value of a number .
  * @c: input
  * Return: return the absolute value of the input .
  */
-int int_abs(int c)
+int int _abs(int c)
 {
 	if (c < 0)
 		return (-c);
