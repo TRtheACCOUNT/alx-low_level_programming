d610dfd0b9a0b9eac1ada2ccab2294968224bef8
Tar <tarmh0559@gmail.com>
2023-03-01 14:00:31 -0800

    project

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* *_strncat - appends two strings.
+* @dest: string.
+* @src: string.
+* @n: number of bytes from src.
+* Return: pointer dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+	i++;
+	}
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	j++;
+	i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* *string_toupper - changes all lowercase letters of a string to uppercase.
+* @str: the string.
+* Return: the reversed array.
+*/
+
+char *string_toupper(char *)
+{
+	int index = 0;
+	while (str[index])
+	{
+	if (str[index] >= 'a' && str[index] <= 'z')
+	str[index] -= 32;
+	index++;
+	}
+	return (str);
+}
